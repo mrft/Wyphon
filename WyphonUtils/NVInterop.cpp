9d63a85ab0a8a04f3424748f60c01351ae4a95d2
Elio <elio@elio.de>
2013-05-20 17:05:43 +0200

    NVInterop: Loading Failiure shall not lead to exception

--- WyphonUtils/NVInterop.cpp
+++ WyphonUtils/NVInterop.cpp
@@ -17,37 +17,44 @@ BOOL loadNvExt() {
 	wglDXOpenDeviceNV = (PFNWGLDXOPENDEVICENVPROC)wglGetProcAddress("wglDXOpenDeviceNV");
 	if(!wglDXOpenDeviceNV)
 	{
-		throw TEXT("wglDXOpenDeviceNV ext is not supported by your GPU or driver.");
+//		throw TEXT("wglDXOpenDeviceNV ext is not supported by your GPU or driver.");
+		return FALSE;
 	}
 	wglDXRegisterObjectNV = (PFNWGLDXREGISTEROBJECTNVPROC)wglGetProcAddress("wglDXRegisterObjectNV");
 	if(!wglDXRegisterObjectNV)
 	{
-		throw TEXT("wglDXRegisterObjectNV ext is not supported by your GPU or driver.");
+//		throw TEXT("wglDXRegisterObjectNV ext is not supported by your GPU or driver.");
+		return FALSE;
 	}
 	wglDXUnregisterObjectNV = (PFNWGLDXUNREGISTEROBJECTNVPROC)wglGetProcAddress("wglDXUnregisterObjectNV");
 	if(!wglDXUnregisterObjectNV)
 	{
-		throw TEXT("wglDXRegisterObjectNV ext is not supported by your GPU or driver.");
+//		throw TEXT("wglDXRegisterObjectNV ext is not supported by your GPU or driver.");
+		return FALSE;
 	}
 	wglDXSetResourceShareHandleNV = (PFNWGLDXSETRESOURCESHAREHANDLENVPROC)wglGetProcAddress("wglDXSetResourceShareHandleNV");
 	if(!wglDXSetResourceShareHandleNV)
 	{
-		throw TEXT("wglDXSetResourceShareHandleNV ext is not supported by your GPU or driver.");
+//		throw TEXT("wglDXSetResourceShareHandleNV ext is not supported by your GPU or driver.");
+		return FALSE;
 	}
 	wglDXLockObjectsNV = (PFNWGLDXLOCKOBJECTSNVPROC)wglGetProcAddress("wglDXLockObjectsNV");
 	if(!wglDXLockObjectsNV)
 	{
-		throw TEXT("wglDXLockObjectsNV ext is not supported by your GPU or driver.");
+//		throw TEXT("wglDXLockObjectsNV ext is not supported by your GPU or driver.");
+		return FALSE;
 	}
 	wglDXUnlockObjectsNV = (PFNWGLDXUNLOCKOBJECTSNVPROC)wglGetProcAddress("wglDXUnlockObjectsNV");
 	if(!wglDXUnlockObjectsNV)
 	{
-		throw TEXT("wglDXUnlockObjectsNV ext is not supported by your GPU or driver.");
+//		throw TEXT("wglDXUnlockObjectsNV ext is not supported by your GPU or driver.");
+		return FALSE;
 	}
 	wglDXCloseDeviceNV = (PFNWGLDXCLOSEDEVICENVPROC)wglGetProcAddress("wglDXCloseDeviceNV");
 	if(!wglDXUnlockObjectsNV)
 	{
-		throw TEXT("wglDXCloseDeviceNV ext is not supported by your GPU or driver.");
+//		throw TEXT("wglDXCloseDeviceNV ext is not supported by your GPU or driver.");
+		return FALSE;
 	}
 
 	return true;
